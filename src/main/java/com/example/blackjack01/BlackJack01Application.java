44077ac769e34ae48608ed83c6ab92244eff2678
user <dafu1985>
2024-02-13 16:01:48 +0900

    blackjack完成

---
+++ src/main/java/com/example/blackjack01/BlackJack01Application.java
@@ -0,0 +1,13 @@
+package com.example.blackjack01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackJack01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackJack01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blackjack01/BlackJack01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blackjack01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackJack01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
